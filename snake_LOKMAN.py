db3b369e9b534161445c539367d245999613edb6
uvsq22002463 <71344615+uvsq22002463@users.noreply.github.com>
2021-05-24 10:14:39 +0200

    Update snake_LOKMAN.py

--- snake_LOKMAN.py
+++ snake_LOKMAN.py
@@ -1,18 +1,42 @@
 
+# projet-snake-
+#2 eme projet : jeu snake 
+
+# groupe BI TD1
+# Hanichi Lokman 
+# Rumer Wilfried 
+# Ami Sada Demya 
+
+# Bieuville Aristide 
+# URL de dépot GitHub : https://github.com/uvsq20001657/projet-snake-
+
 from tkinter import *
 
 from random import randrange
-#la fonction game over ne fonctionne pas
-def game_over ():
-    global mur_bas, mur_droit, mur_gauche , mur_haut , gameover, Serpent
-    if Serpent == mur_droit or mur_bas or mur_haut or mur_gauche:
-        can.delete(all) 
-        can.coords(gameover, 300, 300)
-    elif Serpent == can.create_oval(): 
-        can.delete('all')
-        can.coords(gameover, 300, 300)
+#ajout de coordonnée/intersection pour faciliter le "game_over", donc la fenêtre se fermera grâce à 
+# un fen1.destroy qu'il y'a dans la fonction de lokman grâce au valeurs si dessous  
+# lorsque le serpent se mangera lui même ou sinon dés qu'il touche un des 4murs de wilfried(damya)
+def intersect(Serp,b,can):
 
-def deplacement(): #fonction qui va permettre au serpent d'etre en mouvement de façon automatique et de le faire grandir à chaque fois qu'il mange une pomme.   par lokman
+      bc=can.coords(b)
+      if (Serp[0][0]>= bc[0]  and Serp[0][0]+10 < bc[2]  and Serp[0][1] >= bc[1] and Serp[0][1]+10 < bc[3]):
+        return True
+      else:
+        return False
+def intersectlist(can,a, lb):
+    for b in lb:
+        if intersect(a,b,can):
+            return True
+    return False
+
+def intersecthimself(Serp):
+    for i in range(2 ,len(Serp)):
+        if (Serp[1][0] == Serp[i][0]    and Serp[1][1] == Serp[i][1]) :
+            return True
+    return False
+
+def deplacement(): #fonction qui va permettre au serpent d'etre en mouvement de façon automatique et
+                   # de le faire grandir à chaque fois qu'il mange une pomme.   par lokman
   
     global y,pX,pY,x,Serpent
 
@@ -33,22 +57,27 @@ def deplacement(): #fonction qui va permettre au serpent d'etre en mouvement de 
     mur_gauche = can.create_rectangle((0, 500), (50, 0), fill="saddle brown")
     mur_bas = can.create_rectangle((0, 500), (500, 459), fill="saddle brown")
     mur_haut = can.create_rectangle((0, 0), (500, 50), fill="saddle brown")
-   
+   #destruction du serpent lorsqu'il se touche ou touche un mur par rapport aux intersect que damya a fait plus haut
+    if intersectlist(can ,Serpent,[mur_droit,mur_gauche,mur_bas,mur_haut]) or intersecthimself(Serpent) :
+        
+       
+      fen1.destroy()
+
     if direction  == 'left':
         #le serpent ira a gauche si on appuie sur la flèche de gauche jusqu'à ce qu'il rencontre le mur gauche
-        if Serpent[0][0] > 49:
+        if Serpent[0][0] > 50:
              Serpent[0][0]  = Serpent[0][0] - dx
     elif direction  == 'right':
               #le serpent ira a droite si on appuie sur la fleche de droitejusqu'à ce qu'il rencontre le mur de droite
-        if Serpent[0][0] <445:
+        if Serpent[0][0] <450:
             Serpent[0][0]  = Serpent[0][0] + dx
     elif direction  == 'up':
              #le serpent ira en haut si on appuie sur la fleche du haut jusqu'à ce qu'il rencontre le mur du haut
-        if Serpent[0][1]> 49:
+        if Serpent[0][1]> 50:
             Serpent[0][1]  = Serpent[0][1] - dy
     elif direction  == 'down':
             #le serpent ira en bas si on appuie sur la flèche du bas jusqu'à ce qu'il rencontre le mur du bas
-        if Serpent[0][1] < 451:
+        if Serpent[0][1] < 450:
             Serpent[0][1]  = Serpent[0][1] + dy
 
     #création de la tête du serpent , je change la couleur pour qu'on puisse la differencier du corps 
@@ -69,7 +98,7 @@ def speed():
     global flag
         #cette fonction permet de lancer le jeu dans un premier temps et ensuite d'augmenter la vitesse de déplacement du serpent
     if flag == 0:
-        flag = 1
+        flag =1
     deplacement()
  #on crée les fonctions qui vont permettre de diriger le serpent grâce au flèches du clavier
 def gauche(event):
@@ -101,20 +130,6 @@ def placer_pomme():
             can.coords(pomme,pX, pY, pX+5, pY+5)
              
             Serpent.append([0,0])
-
-
-#la fonction score que nous n'avons pas réussi a faire fonctionner 
-#def score():
- #Label(fen, text='Score:  ').grid(row=0,column=0)
-
- #scores = StringVar()
- #Score = Entry(fen, textvariable=scores)
- #Score.grid(row=0,column=1)
- #scores.set('0')
-    
-    
-
-
 x = 245
 y = 245        
 dx, dy = 10, 10
@@ -128,16 +143,28 @@ Serpent=[[x,y],[x+2.5,y+2.5],[x+5,y+5],[0,0]]
 pX = randrange(50, 450)
 pY = randrange(50, 450)
 
+#la fonction score que nous n'avons pas réussi a faire fonctionner 
+#def score():
+ #Label(fen, text='Score:  ').grid(row=0,column=0)
+
+ #scores = StringVar()
+ #Score = Entry(fen, textvariable=scores)
+ #Score.grid(row=0,column=1)
+ #scores.set('0')
+    
+    
+
+
 #création du canvas par wilfried
 fen1 = Tk()
 can = Canvas(fen1, width=500, height=500, bg='black')
 can.pack(side=BOTTOM, padx=5, pady=5)
 fen1.title(" projet SNAKE ")
-gameover=can.create_text(300,300,text="GAME OVER")
-#on recréé la pomme et les 4murs pour le menu cette fois ci
 
-pomme = can.create_rectangle(pX, pY, pX+5, pY+5,  fill='red')
-mur_droit = can.create_rectangle((450, 500), (500, 0), fill="saddle brown")
+#on recrée la pomme et les 4murs pour le menu cette fois ci
+
+pomme = can.create_rectangle(pX, pY, pX+10, pY+10,  fill='red')
+mur_droit = can.create_rectangle((445, 500), (500, 0), fill="saddle brown")
 mur_gauche = can.create_rectangle((0, 500), (50, 0), fill="saddle brown")
 mur_bas = can.create_rectangle((0, 500), (500, 459), fill="saddle brown")
 mur_haut = can.create_rectangle((0, 0), (500, 50), fill="saddle brown")
@@ -151,17 +178,19 @@ b1.pack(side=LEFT, padx=5, pady=5)
 b2 = Button(fen1, text='Quitter', command=fen1.destroy, bg='black' , fg='white')
 b2.pack(side=RIGHT, padx=5, pady =5)
 
-tex1 = Label(fen1, text="Cliquez une fois sur'speed' pour commencer , cliquez plusieurs fois pour augmenter la vitesse", bg='black' , fg='white')
-tex1.pack(padx=0, pady=11,)
+
 
 #association des touches avec les fonctions de directions
 fen1.bind('<Right>', droite)
 fen1.bind('<Left>', gauche)
 fen1.bind('<Up>' , haut)
 fen1.bind('<Down>', bas)
-
+#affichage des instructions
 ptex1 = Label(fen1, text="utiliser les flèches pour diriger le serpent", bg='black' , fg='white')
 ptex1.pack(padx=0, pady=11,)
+
+tex1 = Label(fen1, text="Cliquez une fois sur'speed' pour commencer , cliquez plusieurs fois pour augmenter la vitesse", bg='black' , fg='white')
+tex1.pack(padx=0, pady=11,)
 
 ptext2 =Label(fen1, text="utiliser la touche quitter pour fermer le jeu", bg='black' , fg='white')
 ptext2.pack(padx=0, pady=11,)
